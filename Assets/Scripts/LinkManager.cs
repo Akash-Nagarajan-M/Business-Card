c07679a34b5733de904d2e80a25ab83a48b795a7
Akash Nagarajan M <86908101+Akash-Nagarajan-M@users.noreply.github.com>
2023-04-30 18:08:53 +0530

    Add files via upload

---
+++ Assets/Scripts/LinkManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+  public void openLinkedin(){
+    Application.OpenURL("https://www.linkedin.com/in/akash-nagarajan-m/");
+  }
+
+  public void openInstagram(){
+     Application.OpenURL("https://www.instagram.com/akash_0115/");
+  }
+
+  public void openDiscord(){
+     Application.OpenURL("https://www.facebook.com/akash.nagarajan.11/");
+  }
+
+  public void openYoutube(){
+     Application.OpenURL("https://www.youtube.com/channel/UC8OyDvzQxISvqtVKLEr_JZg");
+  }
+
+public void openGithub(){
+     Application.OpenURL("https://github.com/Akash-Nagarajan-M");
+  }
+
+}
